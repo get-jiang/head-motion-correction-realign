85a560634b246fd9b277cfaac6f40e31a8546ea9
Xinyue Jiang <jiangxy2022@shanghaitech.edu.cn>
2023-03-17 21:03:23 +0800

    能跑一点，但不多

--- realign.py
+++ realign.py
@@ -1,8 +1,11 @@
 import nibabel as nib
 import numpy as np
-import math
+from math import cos, sin
 from scipy.interpolate import *
 import sympy
+
+# v2d也需要符号式的改写！！！！！！！！！！！
+#但是这里没有
 
 
 def v2d(shape, affine):
@@ -28,15 +31,15 @@ def rigid(q):
                   [0, 0, 0, 1]])
     # 旋转矩阵/Rotation matrix
     R_x = np.array([[1, 0, 0, 0],
-                    [0, math.cos(q[4]), math.sin(q[4]), 0],
-                    [0, -math.sin(q[4]), math.cos(q[4]), 0],
+                    [0, cos(q[4]), sin(q[4]), 0],
+                    [0, -sin(q[4]), cos(q[4]), 0],
                     [0, 0, 0, 1]])
-    R_y = np.array([[math.cos(q[5]), 0, math.sin(q[5]), 0],
+    R_y = np.array([[cos(q[5]), 0, sin(q[5]), 0],
                     [0, 1, 0, 0],
-                    [-math.sin(q[5]), 0, math.cos(q[5]), 0],
+                    [-sin(q[5]), 0, cos(q[5]), 0],
                     [0, 0, 0, 1]])
-    R_z = np.array([[math.cos(q[6]), math.sin(q[6]), 0, 0],
-                    [-math.sin(q[6]), math.cos(q[6]), 0, 0],
+    R_z = np.array([[cos(q[6]), sin(q[6]), 0, 0],
+                    [-sin(q[6]), cos(q[6]), 0, 0],
                     [0, 0, 1, 0],
                     [0, 0, 0, 1]])
     R = R_x @ R_y @ R_z
@@ -74,13 +77,12 @@ def sym_rigid():
     return M
 
 
-def LS(resource, reference):
+def LS(resource, reference,q):
     # 残差函数--最小二乘法  非常慢，需要优化
     # B样条插值/B-spline interpolation
     # 因为找不到三维bspline及其导数的简单库，先用regular grid interpolator代替bspline,用切线斜率代替导数
     interp = RegularGridInterpolator((x, y, z), resource, method="quintic")
     # 对应位置的转换
-
     bi = np.zeros(((shape[0]//step)*(shape[1]//step)*(shape[2]//step)))
     b_diff = np.zeros(
         ((shape[0]//step)*(shape[1]//step)*(shape[2]//step), 7))  # 偏导矩阵
@@ -91,9 +93,11 @@ def LS(resource, reference):
                 n_i = i*step  # 在原图的位置
                 n_j = j*step
                 n_k = k*step
+                M = rigid(q)
                 Mi = M@[n_i, n_j, n_k, 1]
-                Mi_near = [i+1 for i in Mi]
+                
                 if 0 <= Mi[0] < shape[0] and 0 <= Mi[1] < shape[1] and 0 <= Mi[2] < shape[2]:
+                    Mi_near = [i+1 for i in Mi]
                     bi[index] = interp(Mi[:3])-reference[n_i][n_j][n_k]
                     # 切线近似偏导数，分母为1省略
                     diff_x = float(
@@ -102,23 +106,30 @@ def LS(resource, reference):
                         interp(np.append(np.append(Mi[0], Mi_near[1]), Mi[2]))-interp(Mi[:3]))
                     diff_z = float(
                         interp(np.append(Mi[0:2], Mi_near[2]))-interp(Mi[:3]))
-                    for iindex in range(6):
-                        print(diff[iindex+3][0])
-                        
-                        b_diff[index][iindex] = diff_x*(diff[iindex][0].subs('q_'+iindex, q[iindex])) + \
-                            diff_y*(diff[iindex][1].subs('q_'+iindex, q[iindex])) + \
-                            diff_z*(diff[iindex]
-                                    [2].subs('q_'+iindex, q[iindex]))
+
                 else:
                     bi[index] = resource[n_i][n_j][n_k] - \
                         reference[n_i][n_j][n_k]  # 超出范围就不转了
-                    for iindex in range(6):
-                        b_diff[index][iindex] = diff_x*diff[iindex][0] + \
-                            diff_y*diff[iindex][1]+diff_z*diff[iindex][2]
+                    Mi_near = [n_i+1,n_j+1,n_k+1]
+                    diff_x = float(
+                        interp(np.append(Mi_near[0],[ n_j,n_k]))-resource[n_i][n_j][n_k])
+                    diff_y = float(
+                        interp(np.append(np.append([n_i], Mi_near[1]),[n_k]))-resource[n_i][n_j][n_k])
+                    diff_z = float(
+                        interp(np.append([n_i,n_j], Mi_near[2]))-resource[n_i][n_j][n_k])
+                ##很丑的代码
+                b_diff[index][0] = diff_x
+                b_diff[index][1] = diff_y
+                b_diff[index][2] = diff_z
+                b_diff[index][3] = diff_y*(-10.33*sin(q[4])*sin(q[6]) + 2.33*sin(q[4])*cos(q[6]) + 2.33*sin(q[5])*sin(q[6])*cos(q[4]) + 10.33*sin(q[5])*cos(q[4])*cos(q[6]) - 6.77*cos(q[4])*cos(
+                        q[5])) + diff_z*(-6.97*sin(q[4])*sin(q[5])*sin(q[6]) - 30.90*sin(q[4])*sin(q[5])*cos(q[6]) + 20.26*sin(q[4])*cos(q[5]) - 30.90*sin(q[6])*cos(q[4]) + 6.97*cos(q[4])*cos(q[6]))
+                b_diff[index][4] = diff_x*(7.0*sin(q[5])*sin(q[6]) + 31.0*sin(q[5])*cos(q[6]) - 20.33*cos(q[5]))+diff_y*(6.77*sin(q[4])*sin(q[5]) + 2.33*sin(q[4])*sin(q[6])*cos(
+                        q[5]) + 10.33*sin(q[4])*cos(q[5])*cos(q[6])) + diff_z*(20.26*sin(q[5])*cos(q[4]) + 6.97*sin(q[6])*cos(q[4])*cos(q[5]) + 30.906*cos(q[4])*cos(q[5])*cos(q[6]))
+                b_diff[index][5] = diff_x*(31.0*sin(q[6])*cos(q[5]) - 7.0*cos(q[5])*cos(q[6]))+diff_y*(-10.33*sin(q[4])*sin(q[5])*sin(q[6]) + 2.33*sin(q[4])*sin(q[5])*cos(q[6]) + 2.33*sin(q[6])*cos(
+                        q[4]) + 10.33*cos(q[4])*cos(q[6]))+diff_z*(-6.97*sin(q[4])*sin(q[6]) - 30.90*sin(q[4])*cos(q[6]) - 30.90*sin(q[5])*sin(q[6])*cos(q[4]) + 6.97*sin(q[5])*cos(q[4])*cos(q[6]))
                 b_diff[index][6] = -reference[n_i][n_j][n_k]
                 index += 1
-    print(b_diff)
-    return bi
+    return bi,b_diff
 
 
 # -----------------------------------------------
@@ -158,7 +169,10 @@ diff = np.array([diff_1, diff_2, diff_3, diff_4,
 
 # 高斯牛顿迭代
 # 算残差函数
-# 取一张图试试先
-step = 10  # 因为跑的慢所以不要每个点都跑，先选一些试试
-b = LS(img_data[:, :, :, 1], img_data[:, :, :, 2])
-# print(b)
+step = 20  # 因为跑的慢所以不要每个点都跑，先选一些试试
+for picture in range(1,shape[3]):
+    for i in range(4):
+        (b,diff_b) = LS(img_data[:, :, :, 0], img_data[:, :, :, picture],q)
+        q -=np.linalg.inv(diff_b.T@diff_b)@diff_b.T@b
+        print(q)
+        print(sum(b))
